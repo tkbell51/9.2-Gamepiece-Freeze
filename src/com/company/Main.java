28be4ac51322ea2cc4e37a2007bd66959af98cc5
tkbell51 <tkbell51@gmail.com>
2017-07-21 10:24:52 -0400

    Complete Assignment with made a game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-//        System.out.print("Hello! Ready to play Freeze? ");
+        System.out.print("Hello! Ready to play Freeze? ");
         Scanner scanner = new Scanner(System.in);
-//        String gameAnswer = scanner.nextLine();
-//
-//        if (gameAnswer.equalsIgnoreCase("Y") || gameAnswer.equalsIgnoreCase("Yes")) {
-//            System.out.println("Great! Let's Play");
-//        } else {
-//            System.out.println("Hmmm. Let's play anyway.");
-//        }
+        String gameAnswer = scanner.nextLine();
+
+        if (gameAnswer.equalsIgnoreCase("Y") || gameAnswer.equalsIgnoreCase("Yes")) {
+            System.out.println("Great! Let's Play");
+        } else {
+            System.out.println("Hmmm. Let's play anyway.");
+        }
         System.out.println("FREEZE!!!");
         System.out.print("Name: ");
         String name = scanner.nextLine();
@@ -25,17 +25,36 @@ public class Main {
         myGamePiece.setName(name);
         myGamePiece.printInfo();
 
-        myGamePiece.move();
-        myGamePiece.move();
-        myGamePiece.freeze();
-        myGamePiece.move();
-        myGamePiece.move();
-        myGamePiece.unfreeze();
-        myGamePiece.move();
-        myGamePiece.move();
+//        myGamePiece.move();
+//        myGamePiece.move();
+//        myGamePiece.freeze();
+//        myGamePiece.move();
+//        myGamePiece.move();
+//        myGamePiece.unfreeze();
+//        myGamePiece.move();
+//        myGamePiece.move();
 
         //while loop to play the game until this.counter == ?20 or this.hearts == 0
+        while(myGamePiece.getHearts() != 0 && myGamePiece.getCounter() != 20){
+            Random rand = new Random();
+            System.out.println("Press Enter to move ");
+            scanner.nextLine();
+            myGamePiece.move();
+            if(myGamePiece.getCounter()==rand.nextInt(20)){
+                myGamePiece.freeze();
+                myGamePiece.move();
+                System.out.println("Press Enter to unfreeze");
+                scanner.nextLine();
+                myGamePiece.unfreeze();
+            }
+            if(myGamePiece.getHearts() == 0){
+                System.out.println("Freeze!! You Lose");
+            }
+            if (myGamePiece.getCounter() == 20){
+                System.out.println("You Win!!");
+            }
 
+        }
 
 
 
